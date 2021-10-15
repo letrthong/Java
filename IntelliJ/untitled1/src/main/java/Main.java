68d85ae62c420b1795b939c7a5707d159ae2db06
Thong LT <letrthong@gmail.com>
2021-10-15 17:09:48 +0700

    Twilio

---
+++ IntelliJ/untitled1/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+
+public class Main {
+    public static final String accountSid ="AC944d62f5924b8ac8209d63051e37544d";
+    public static final String authToken = "fe43d921803da431f71ee7a613cb7xxx";
+
+    public static void main(String[] args) {
+        Twilio.init(accountSid, authToken);
+        String whats_app_form = "+14155238886";
+
+        //USERID
+        String whats_app_to = "+84356148xxx";
+
+        Message message = Message.creator(
+                        new com.twilio.type.PhoneNumber("whatsapp:" + whats_app_to ),
+                        new com.twilio.type.PhoneNumber("whatsapp:" + whats_app_form ),
+                        "Hello there!")
+                .create();
+
+
+        System.out.println("Status=" +  message.getStatus());
+        System.out.println("sid=" + message.getSid());
+        System.out.println("Price=" + message.getPrice());
+        System.out.println("PriceUnit=" + message.getPriceUnit());
+    }
+}
