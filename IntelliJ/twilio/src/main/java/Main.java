ef210004b629e1305deb2f8d21576698ea3babd8
Thong LT <letrthong@gmail.com>
2021-10-15 17:20:59 +0700

    clean up

--- IntelliJ/twilio/src/main/java/Main.java
+++ IntelliJ/twilio/src/main/java/Main.java
@@ -3,14 +3,14 @@ import com.twilio.rest.api.v2010.account.Message;
 
 public class Main {
     public static final String accountSid ="AC944d62f5924b8ac8209d63051e37544d";
-    public static final String authToken = "fe43d921803da431f71ee7a613cb7xxx";
+    public static final String authToken = "fe43d921803da431f71ee7a613cbxxxx";
 
     public static void main(String[] args) {
         Twilio.init(accountSid, authToken);
         String whats_app_form = "+14155238886";
 
         //USERID
-        String whats_app_to = "+84356148xxx";
+        String whats_app_to = "+8435614xxxx";
 
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber("whatsapp:" + whats_app_to ),
